09e43a17e631e01aaa0e503fcf3d6fdc549321b9
Danilin Aleksandr <dani071106@gmail.com>
2024-05-13 02:36:14 +0300

    Initial commit

--- dj/settings.py
+++ dj/settings.py
@@ -91,10 +91,10 @@ WSGI_APPLICATION = 'dj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'T0mka$default',
-        'USER': 'T0mka',
+        'NAME': 'Danilin$default',
+        'USER': 'Danilin',
         'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': 'T0mka.mysql.pythonanywhere-services.com',
+        'HOST': 'Danilin.mysql.pythonanywhere-services.com',
         'OPTIONS': {
             'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
